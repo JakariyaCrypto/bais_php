4dd3e250f0ddbed84dd970e8bdcb9c92819bb9ea
JakariyaCrypto <developerjk9356@gmail.com>
2023-06-13 12:14:04 +0600

    baisc login with php

--- basic/index.php
+++ basic/index.php
@@ -3,27 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>form</title>
 </head>
 <body>
+    <form action="index.php"  method="post">
+        <lebel>User Name</lebel>
+        <input type="text" name="user_name" ><br>
+        <lebel>Password</lebel>
+        <input type="password" name="password" ><br>
+        <input type="submit" name="login" value="Login">
+    </form>
 
 
-<?php
-    $countries = array(
-        "japan" => "japan",
-        "Bangladesh" => "BD",
-        "usa" => "United South Asia",
-        "uk" => "United Kingdom",
-    );
+    <?php
 
-    foreach($countries as $kay => $value){
-        echo "{$kay} = {$value}";
+    // foreach($_POST as $key => $val){
+    //     echo "{$key} = $val <br>";
+    // }
+
+    if(isset($_POST['login'])){
+        $userName = $_POST['user_name'];
+        $password = $_POST['password'];
+
+        if(empty($userName)){
+            echo "userName is not match";
+        }elseif(empty($password)){
+            echo "password is not match";
+        }else{
+            echo "Suucessfully login";
+        }
     }
-
-?>
-
-<script>
-
-</script>
+    ?>
 </body>
 </html>
